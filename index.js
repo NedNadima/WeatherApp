f494592abac018dbf45cc39dcb3b63c7fe05995a
Ned <nnadima1@gmail.com>
2022-09-12 17:45:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Baby weather app
+// if raining, wear a jacket
+// if not, wear sunglasses
+
+let weather = prompt('How is the weather?')
+  if (weather == 'rain'){
+    console.log('Grab your rain jacket')
+  } else {console.log ('Grab your sunglasses')}
